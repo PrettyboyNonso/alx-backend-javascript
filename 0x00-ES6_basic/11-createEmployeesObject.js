43e34cad2e4d4c87d103318c024e90c55991f343
PrettyboyNonso <obii6776@gmail.com>
2024-10-04 11:36:19 -0700

    task 11

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,9 @@
+export default function createEmployeesObject(departmentName, employees) {
+  if (typeof departmentName !== 'string') {
+    throw new Error('departmentName must be a type string');
+  }
+  if (!Array.isArray(employees)) {
+    throw new Error('employees must be a type array');
+  }
+  return { [`${departmentName}:`]: employees };
+}
